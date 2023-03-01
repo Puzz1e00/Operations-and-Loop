438287c3da727035f506951182d5781f0885ba52
Puzz1e00 <baibhavacharya10@gmail.com>
2023-03-01 16:04:37 +0545

    First task

---
+++ task1.py
@@ -0,0 +1,7 @@
+
+x1= int(input("Enter first x coordinate"))
+x2= int(input("Enter second x coordinate"))
+y1= int(input("Enter first y coordinate"))
+y2= int(input("Enter second y coordinate"))
+d=((x2-x1)**2+(y2-y1)**2)**1/2
+print(d)
